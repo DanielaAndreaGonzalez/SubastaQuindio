a118ee2d6a1de09703d140cf44f1eebb7670cc89
DanielaAndreaGonzalez <daniela.andrea.gonzalez@correounivalle.edu.co>
2022-11-22 23:18:17 -0500

    Se actualiza servidor para perisistir anuncios y se agrega btn descargar reporte anuncios

--- SubastaQuindio/co.edu.uniquindio.subastaQuindio.Servidor/src/co/edu/uniquindio/subastaQuindio/models/Anuncio.java
+++ SubastaQuindio/co.edu.uniquindio.subastaQuindio.Servidor/src/co/edu/uniquindio/subastaQuindio/models/Anuncio.java
@@ -4,6 +4,7 @@
 package co.edu.uniquindio.subastaQuindio.models;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
  * @author GonzalezHDanielaA
@@ -11,8 +12,77 @@ import java.io.Serializable;
  */
 public class Anuncio implements Serializable{
 	
-	private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 	
+	private LocalDate fechaPublicacion;
+	private LocalDate fechaLimitePublicacion;
+	private Producto producto;
+	private Persona anunciante;
+
 	public Anuncio() {}
+
+	/**
+	 * @return the fechaPublicacion
+	 */
+	public LocalDate getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	/**
+	 * @param fechaPublicacion the fechaPublicacion to set
+	 */
+	public void setFechaPublicacion(LocalDate fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+	/**
+	 * @return the fechaLimitePublicacion
+	 */
+	public LocalDate getFechaLimitePublicacion() {
+		return fechaLimitePublicacion;
+	}
+
+	/**
+	 * @param fechaLimitePublicacion the fechaLimitePublicacion to set
+	 */
+	public void setFechaLimitePublicacion(LocalDate fechaLimitePublicacion) {
+		this.fechaLimitePublicacion = fechaLimitePublicacion;
+	}
+	
+	
+
+	/**
+	 * @return the producto
+	 */
+	public Producto getProducto() {
+		return producto;
+	}
+
+	/**
+	 * @param producto the producto to set
+	 */
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	/**
+	 * @return the anunciante
+	 */
+	public Persona getAnunciante() {
+		return anunciante;
+	}
+
+	/**
+	 * @param anunciante the anunciante to set
+	 */
+	public void setAnunciante(Persona anunciante) {
+		this.anunciante = anunciante;
+	}
+
+	@Override
+	public String toString() {
+		return "Anuncio [fechaPublicacion=" + fechaPublicacion + ", fechaLimitePublicacion=" + fechaLimitePublicacion
+				+ ", producto=" + producto + ", anunciante=" + anunciante + "]";
+	}
 
 }
